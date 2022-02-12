87a96329b965faf1a0c98041057326a321a401a9
Daniel Bahrami <daniel@bahrami.dk>
2022-02-12 22:43:17 +0100

    Function for counting cycles

--- main.cpp
+++ main.cpp
@@ -7,20 +7,36 @@ void permutation(int n) {
     for (int i = 0; i < n; i++) {
         vector.push_back(i);
     }
-
     std::srand(time(0));
     for (int i = 0; i < vector.size(); i++) {
         int j = i + rand() % (vector.size() - i);
         std::swap(vector[i], vector[j]);
     }
-
     for (int i = 0; i < vector.size(); i++) {
         std::cout << vector[i] << " ";
     }
     std::cout << std::endl;
 }
 
+int countCycles(std::vector<int> vector) {
+    int count = 0;
+    std::vector<int> checked;
+    for (int i = 0; i < vector.size(); i++) {
+        if (std::find(checked.begin(), checked.end(), i) != checked.end()) {
+            continue;
+        }
+        for (int j = vector[i]; j != i; j = vector[j]) {
+            checked.push_back(j);
+        }
+        count++;
+    }
+    return count;
+}
+
 int main() {
+    std::cout << "Random permutation:" << std::endl;
     permutation(10);
+    std::cout << "Number of cycles:" << std::endl;
+    std::cout << countCycles(vector) << std::endl;
     return 0;
 }
