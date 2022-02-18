ecb28584a7b044dd5dbb5f031deea09176bcc75c
Daniel Bahrami <daniel@bahrami.dk>
2022-02-18 09:58:37 +0100

    Fix Merge Sort

--- Merge Sort/main.cpp
+++ Merge Sort/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
 
-void merge(std::vector<int> A, int p, int q, int r) {
-    int n1 = q - p + 1;
+void merge(std::vector<int> &A, int p, int q, int r) {
+    int n1 = q - p;
     int n2 = r - q;
     std::vector<int> L;
     std::vector<int> R;
     for (int i = 0; i < n1; i++) {
-        L.push_back(A[p + i - 1]);
+        L.push_back(A[p + i]);
     }
     for (int j = 0; j < n2; j++) {
         R.push_back(A[q + j]);
@@ -27,17 +27,20 @@ void merge(std::vector<int> A, int p, int q, int r) {
     }
 }
 
-void mergeSort(std::vector<int> A, int p, int r) {
-    if (p < r) {
-        int q = p + r / 2;
+void mergeSort(std::vector<int> &A, int p, int r) {
+    if (p < r - 1) {
+        int q = (p + r) / 2;
         mergeSort(A, p, q);
-        mergeSort(A, q + 1, r);
+        mergeSort(A, q, r);
         merge(A, p, q, r);
     }
 }
 
 int main() {
-    std::vector<int> vector = {45, 46, 75, 66, 17, 93, 11, 95, 87, 55};
+    std::vector<int> vector = {100, 46, 75, 66, 17, 93, 11, 95, 87, 55, 3332, 2, 9, -9};
     mergeSort(vector, 0, vector.size());
+    for (int i = 0; i < vector.size(); i++) {
+        std::cout << vector[i] << " ";
+    }
     return 0;
 }
