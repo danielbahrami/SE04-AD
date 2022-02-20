3fa09ad31713e611d8c844fde3e896c7bd2a0985
Daniel Bahrami <daniel@bahrami.dk>
2022-02-20 22:13:35 +0100

    Update Insertion Sort

--- Insertion Sort/main.cpp
+++ Insertion Sort/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> vector;
-
-void permutation(int n) {
+void permutation(std::vector<int> &vector, int n) {
     for (int i = 0; i < n; i++) {
         vector.push_back(i);
     }
@@ -12,36 +10,38 @@ void permutation(int n) {
         int j = i + rand() % (vector.size() - i);
         std::swap(vector[i], vector[j]);
     }
+}
+
+void print(std::vector<int> vector) {
     for (int i = 0; i < vector.size(); i++) {
         std::cout << vector[i] << " ";
     }
     std::cout << std::endl;
 }
 
-void insertionSort(std::vector<int> vector) {
+void insertionSort(std::vector<int> &vector) {
     for (int i = 1; i < vector.size(); i++) {
         int key = vector[i];
         int j = i - 1;
         while (j >= 0 && vector[j] > key) {
             vector[j + 1] = vector[j];
-            j = j - 1;
+            j--;
         }
         vector[j + 1] = key;
     }
-    for (int i = 0; i < vector.size(); i++) {
-        std::cout << vector[i] << " ";
-    }
-    std::cout << std::endl;
 }
 
 int main() {
+    std::vector<int> vector;
     std::cout << "Random permutation:" << std::endl;
-    permutation(100);
+    permutation(vector, 100);
+    print(vector);
     std::cout << "Insertion Sort:" << std::endl;
     auto start = std::chrono::high_resolution_clock::now();
     insertionSort(vector);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    print(vector);
     std::cout << "Execution time of Insertion Sort:" << std::endl;
     std::cout << duration.count() << " microseconds" << std::endl;
     return 0;
